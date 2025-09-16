780e29d233c5bd4c3af2d58dd608fc332f0cc3b0
Shahad It <shahadalrehily@gmail.com>
2025-09-16 07:47:41 +0000

    kick off olist challenge

--- olist/data.py
+++ olist/data.py
@@ -1,23 +1,25 @@
-import os
-import pandas as pd
-
-
 class Olist:
     def get_data(self):
-        """
-        This function returns a Python dict.
-        Its keys should be 'sellers', 'orders', 'order_items' etc...
-        Its values should be pandas.DataFrames loaded from csv files
-        """
-        # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
-            # Do not hardcode your path as it only works on your machine ('Users/username/code...')
-            # Use __file__ instead as an absolute path anchor independant of your usename
-            # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
-        # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+        import os
+        import pandas as pd
+
+        # بناء المسار المطلق لمجلد csv
+        base_path = os.path.dirname(__file__)
+        csv_path = os.path.join(base_path, 'data', 'csv')
+
+        # قائمة أسماء الملفات
+        file_names = [f for f in os.listdir(csv_path) if f.endswith('.csv')]
+
+        # تحويل أسماء الملفات لمفاتيح dictionary
+        key_names = [f.replace('olist_', '').replace('_dataset.csv', '').replace('.csv','') for f in file_names]
+
+        # قراءة CSVs وبناء dictionary
+        data = {}
+        for key, file in zip(key_names, file_names):
+            data[key] = pd.read_csv(os.path.join(csv_path, file))
+
+        return data
 
     def ping(self):
-        """
-        You call ping I print pong.
-        """
         print("pong")
+
